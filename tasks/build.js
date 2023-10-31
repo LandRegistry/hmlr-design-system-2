ba796800f4830ca9ef3bf9d3a181e7b5f610d189
Martin Coulthurst <mc@lightness.co.uk>
2023-10-31 18:11:23 +0000

    Add metalsmith-assets plugin to add custom 404 html page; suppress ie8 warnings from govuk

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,13 @@
 const metalsmith = require('../lib/metalsmith') // configured static site generator
 
+var assets = require('metalsmith-assets');
+
+// use metalsmith-assets to place 404 page in the root in order to work with http-server magic
+metalsmith.use(assets({
+  source: './assets', // relative to the working directory
+  destination: './' // relative to the build directory
+}))
+
 // build to destination directory
 metalsmith.build(function (err, files) {
   if (err) { throw err }
